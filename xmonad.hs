115cdc6c58e9f126ca786cb0e72ca5620908af53
Bas Bossink <bas.bossink@gmail.com>
2010-05-20 02:03:37 +0200

    Giving up on xmobar.

--- xmonad.hs
+++ xmonad.hs
@@ -10,13 +10,8 @@ myManageHook = composeAll
     ]
 
 main = do
-    xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
         , layoutHook = avoidStruts $ layoutHook defaultConfig
-        , logHook = dynamicLogWithPP $ xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
         , modMask = mod4Mask --Rebind Mod to Windows key
         } 
